e3b9c07e51e8918de097f585d700ed94495f3f13
Jesus-Aranda <arandacardenasjesushumberto@gmail.com>
2025-10-08 17:11:25 -0600

    Add files via upload

---
+++ EVA2_14_ARREGLOS_FOR/src/eva2_14_arreglos_for/EVA2_14_ARREGLOS_FOR.java
@@ -0,0 +1,14 @@
+package eva2_14_arreglos_for;
+
+public class EVA2_14_ARREGLOS_FOR {
+
+    public static void main(String[] args) {
+        int arreglo[] = new int[10];
+        
+        for (int i = 0; i < arreglo.length; i++) {arreglo[i] = (int)(Math.random()*100);}
+                
+        for (int i = 0; i < arreglo.length; i++) {System.out.print("[" + arreglo[i] + "]");}
+        
+        System.out.println();
+     }
+}
